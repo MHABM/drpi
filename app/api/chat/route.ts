24a5c2bdde2840d061f28a2b2398237467ef2bad
MHABM <mhabm2th@gmail.com>
2024-07-19 15:59:52 +0330

    gpt4

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = await streamText({
-    model: openai('gpt-3.5-turbo-0125'),
+    model: openai('gpt-4o'),
     messages: [
         {role: 'system',content: 'You always answer in persian language.You go straight to the point, your replies are under 500 characters. You just answer questions about health, medical or dentistry.'},
         ...messages
